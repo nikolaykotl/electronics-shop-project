df14b807cd77b60cb2015e981eb9149f7447cc82
nikolay <nikkot1981@gmail.com>
2023-07-28 09:32:51 +0400

    Реализован класс Item в src/item.py добавлены тесты в tests/test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
+import pytest
+from src.item import Item
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+
+@pytest.fixture
+def item():
+    return Item('ноутбук', 50000, 10)
+
+
+def test_item_calculate_total_price(item):
+    assert item.calculate_total_price() == 500000
+
+def test_item_apply_discount(item):
+     Item.pay_rate = 0.9
+     assert item.apply_discount() == 45000
